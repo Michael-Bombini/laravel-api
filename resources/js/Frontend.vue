e74c1e99856113e1396299db68ffe3312db3b480
Michael Bombini <bombini.michael@gmail.com>
2022-08-29 15:38:23 +0200

    Added axios api call

--- resources/js/Frontend.vue
+++ resources/js/Frontend.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script>
+import Axios from 'axios';
+
+
     export default {
         mounted() {
-            console.log('Mounted!');
+            Axios.get('/Api/posts')
+            .then((resp) => {
+                console.log(resp.data);
+
+            });
 
         }
     }
